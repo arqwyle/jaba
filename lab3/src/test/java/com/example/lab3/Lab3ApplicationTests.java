9228b15414df87795688c5d7a4e095036d7e605f
Yuriy Katokhin <arqwyle@gmail.com>
2025-03-09 14:58:17 +0700

    create lab3 test

--- lab3/src/test/java/com/example/lab3/Lab3ApplicationTests.java
+++ lab3/src/test/java/com/example/lab3/Lab3ApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.lab3;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AuthorController.class)
 class Lab3ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@Test
+	public void testGetAuthorInfo() throws Exception {
+		mockMvc.perform(get("/author"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("author"))
+				.andExpect(model().attribute("authorName", "Юрий Алексеевич Катохин"));
+	}
 }
