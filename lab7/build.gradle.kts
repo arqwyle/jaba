3e784959259230a8d133c9b583add5575d6155d6
Yuriy Katokhin <arqwyle@gmail.com>
2025-04-06 11:56:06 +0700

    sort dependencies

--- lab7/build.gradle.kts
+++ lab7/build.gradle.kts
@@ -21,12 +21,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.projectlombok:lombok")
-	implementation("org.liquibase:liquibase-core")
 	annotationProcessor("org.projectlombok:lombok")
+	implementation("org.liquibase:liquibase-core")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.testcontainers:postgresql:1.18.3")
-	testImplementation("org.testcontainers:junit-jupiter:1.18.3")
+	testImplementation("org.testcontainers:postgresql")
+	testImplementation("org.testcontainers:junit-jupiter")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
